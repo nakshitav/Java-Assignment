a5b3389965c74b8dcd3092e419ea5039c99eff8a
nakshitav <116201603+nakshitav@users.noreply.github.com>
2022-10-20 22:20:34 +0530

    Delete TimeStampToDate.java

--- TimeStampToDate.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Timestamp;    
-import java.util.Date;    
- public class TimestampToDate {    
-       public static void main(String args[]){    
-                Timestamp ts=new Timestamp(System.currentTimeMillis());  
-                Date date=new Date(ts.getTime());  
-                System.out.println(date);                     
-        }    
-}    
